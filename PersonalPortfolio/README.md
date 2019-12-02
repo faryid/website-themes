3a0ebe42db92bb1ff8438fc791deab146d3e6f2e
faryid <m.akbar.alfaryid@gmail.com>
2019-12-02 12:18:21 +0700

    Add PersonalPortfolio V1

---
+++ PersonalPortfolio/README.md
@@ -0,0 +1,25 @@
+# PersonalPortfolio
+
+## Fitur-fitur yang sudah tersedia:
+
+✅ Navigation Menu - Menu hamburger untuk mobile
+
+✅ Hero Section - Tampilan menarik di halaman utama
+
+✅ About Section - Profil dan statistik kinerja
+
+✅ Projects Gallery - Showcase proyek Anda
+
+✅ Skills Section - Daftar keahlian dengan icon
+
+✅ Contact Form - Formulir kontak interaktif
+
+✅ Social Links - Tombol media sosial
+
+## Tambahan fitur untuk gaya tampilan:
+
+✅ Responsive Design - Sempurna di mobile, tablet, dan desktop
+
+✅ Mobile Menu - Toggle menu untuk perangkat mobile
+
+✅ Smooth Animations - Efek animasi saat scroll
---
+++ PersonalPortfolio/script.js
@@ -0,0 +1,60 @@
+// Mobile Menu Toggle
+const menuToggle = document.querySelector('.menu-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+menuToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
+
+// Close menu when a link is clicked
+document.querySelectorAll('.nav-menu a').forEach(link => {
+    link.addEventListener('click', () => {
+        navMenu.classList.remove('active');
+    });
+});
+
+// Smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            target.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
+
+// Form submission
+const contactForm = document.querySelector('.contact-form');
+if (contactForm) {
+    contactForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+        alert('Terima kasih! Pesan Anda telah dikirim.');
+        this.reset();
+    });
+}
+
+// Scroll animation for elements
+const observerOptions = {
+    threshold: 0.1,
+    rootMargin: '0px 0px -100px 0px'
+};
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.style.opacity = '1';
+            entry.target.style.transform = 'translateY(0)';
+        }
+    });
+}, observerOptions);
+
+document.querySelectorAll('.project-card, .skill-item, .stat').forEach(el => {
+    el.style.opacity = '0';
+    el.style.transform = 'translateY(20px)';
+    el.style.transition = 'all 0.5s ease';
+    observer.observe(el);
+});
