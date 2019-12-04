05d2c4c374bfd40175b749483a696ff9e83e9a44
faryid <m.akbar.alfaryid@gmail.com>
2019-12-04 09:24:16 +0700

    Add Work or Home page

---
+++ 2019/portfolio/avana/react/js/src/pages/Work.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../layout/Header';
+import Main from '../layout/Main';
+import Footer from '../layout/Footer';
+
+function Work() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default Work;
